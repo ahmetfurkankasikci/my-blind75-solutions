9bb1885272bef4ef9d5d84d12aed760e10b130d2
kashich <furkankasikci45@gmail.com>
2022-12-10 20:59:40 +0300

    palindrome sadeleştirildi

--- 09-valid_palindrome/solution.js
+++ 09-valid_palindrome/solution.js
@@ -1,5 +1,3 @@
 var isPalindrome = function(s) {
-    var punctuationless = s.replace(/[.,"@['\]/#!$%?\^&\*;:{}=\-_`~()]/g,"");
-    var finalString = punctuationless.replace(/\s{2,}/g," ");
-    return finalString.toLowerCase().replace(/\s/g, '')===finalString.split("").reverse().join(" ").toLowerCase().replace(/\s/g, '');
+    return s.replace(/[.,"@['\]/#!$%?\^&\*;:{}=\-_`~()\s]/g,"").toLowerCase()===s.replace(/[.,"@['\]/#!$%?\^&\*;:{}=\-_`~()\s]/g,"").split("").reverse().join("").toLowerCase();
 };
